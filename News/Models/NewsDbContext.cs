83cd91ebccddd991c46b3b2908d5b711845a3de2
ВинБоцы ВинБоцы <solarisraver@mail.ru>
2023-05-31 20:08:05 +0600

    Добавьте файлы проекта.

---
+++ News/Models/NewsDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace News.Models
+{
+    public class NewsDbContext : DbContext
+    {
+        public NewsDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Post> Posts { get; set; }
+    }
+}
