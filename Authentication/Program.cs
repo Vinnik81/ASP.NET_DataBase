47b454b9d1b1c8aa437e3b199c0f07e73235698e
ВинБоцы ВинБоцы <solarisraver@mail.ru>
2023-07-09 19:40:43 +0600

    1

--- Authentication/Program.cs
+++ Authentication/Program.cs
@@ -34,6 +34,14 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+app.Use(async (context, next) =>
+{
+    if (context.Request.Cookies.Count > 0)
+    {
+        await next.Invoke();
+    }
+});
 
-//app.UseMiddleware<KeyMiddleware>();
+
+app.UseMiddleware<KeyMiddleware>();
 app.Run();
