244bd04763386f9b50f61ce32283e10bf55ef933
ВинБоцы ВинБоцы <solarisraver@mail.ru>
2023-07-10 22:01:44 +0600

    HomeWork

--- Authentication/Program.cs
+++ Authentication/Program.cs
@@ -34,5 +34,5 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-//app.UseMiddleware<KeyMiddleware>();
+app.UseMiddleware<KeyMiddleware>();
 app.Run();
